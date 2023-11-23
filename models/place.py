37092f42e552c6484bbffcbac4b8666cf7d5a5e1
AhmedNewiry <ahmedabdalfttah@outlook.com>
2023-11-23 03:43:57 -0800

    fix

--- models/place.py
+++ models/place.py
@@ -30,7 +30,10 @@ class Place(BaseModel, Base):
         price_by_night = Column(Integer, default=0, nullable=False)
         latitude = Column(Float)
         longitude = Column(Float)
-        reviews = relationship("Review", backref="place", cascade="delete")
+        reviews = relationship(
+            "Review",
+            backref="place",
+            cascade="all, delete, delete-orphan")
         amenities = relationship("Amenity", secondary="place_amenity",
                                  backref="place_amenities", viewonly=False)
 
--- models/state.py
+++ models/state.py
@@ -12,7 +12,10 @@ class State(BaseModel, Base):
     if models.st_type == "db":
         __tablename__ = "states"
         name = Column(String(128), nullable=False)
-        cities = relationship("City", backref="state", cascade="all, delete, delete-orphan")
+        cities = relationship(
+            "City",
+            backref="state",
+            cascade="all, delete, delete-orphan")
 
     else:
         name = ""
