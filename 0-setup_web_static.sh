0879e78268c81964f194c31a0dd87c9f6b697b8c
AhmedNewiry <ahmedabdalfttah@outlook.com>
2023-12-07 13:52:20 -0800

    fix

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -18,5 +18,5 @@ fi
 sudo ln -sf  /data/web_static/releases/test/ /data/web_static/current
 
 sudo chown -R ubuntu:ubuntu /data/
-sudo sed -i '52 a\   	location /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-enabled/default
+sudo sed -i "/server_name _;/a  location /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}" /etc/nginx/sites-enabled/default
 sudo service nginx restart
