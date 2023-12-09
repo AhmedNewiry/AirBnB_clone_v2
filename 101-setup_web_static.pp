2c1bcf1141060492add14acc12f5f9166f2a885f
AhmedNewiry <ahmedabdalfttah@outlook.com>
2023-12-09 14:28:35 -0800

    fix puppet

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -2,22 +2,27 @@
 
 $dir_list = ['/data', '/data/web_static', '/data/web_static/releases', '/data/web_static/releases/test']
 $nginx_conf = "server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
-    add_header X-Served-By $hostname;
-    root   /var/www/html;
-    index  index.html index.htm;
-    location /hbnb_static {
-        alias /data/web_static/current/;
-    }
-    location /redirect_me {
-        return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
-    }
-    error_page 404 /404.html;
-    location /404 {
-      root /var/www/html;
-      internal;
-    }
+        listen 80 default_server;
+        listen [::]:80 default_server;
+        root /var/www/html;
+        index index.html index.htm index.nginx-debian.html;
+        server_name _;
+        location /hbnb_static {
+                alias /data/web_static/current/;
+        }
+        add_header X-Served-By \$hostname;
+        location / {
+                # First attempt to serve request as file, then
+                # as directory, then fall back to displaying a 404.
+                try_files \$uri \$uri/ =404;
+        }
+        error_page 404 /not_found.html;
+        location /not_found.html {
+                internal;
+        }
+        location /redirect_me {
+                return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
+        }
 }
 "
 exec {'update repo resources':
@@ -27,7 +32,7 @@ exec {'update repo resources':
 package { 'nginx':
     ensure => 'present',
     provider => 'apt'
-}
+}->
 file { $dir_list:
     ensure => 'directory',
     owner => 'ubuntu',
@@ -47,11 +52,11 @@ file {'/data/web_static/releases/test/index.html':
     group => 'ubuntu',
 }->
 file {'/data/web_static/current':
-    ensure => 'link', 
+    ensure => 'link',
     target => '/data/web_static/releases/test'
 }->
 
-file { '/etc/nginx/sites-enabled/default':
+file { '/etc/nginx/sites-available/default':
     ensure => 'present',
     content => $nginx_conf
 }->
